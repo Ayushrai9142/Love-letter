1eb7fdfa2adc8f6390e00d09cb6f888ae5ab22b3
Ayushrai9142 <ayushrai7533@gmail.com>
2025-03-16 19:30:27 +0530

    Update sendRecipientDetails.js

--- functions/sendRecipientDetails.js
+++ functions/sendRecipientDetails.js
@@ -1,24 +1,24 @@
 const nodemailer = require("nodemailer");
 
 exports.handler = async (event) => {
-    const formData = JSON.parse(event.body);
-
-    let transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: "ayushrai7533@gmail.com",
-            pass: "byiz iopt ceyr vzzy"
-        }
-    });
-
-    let mailOptions = {
-        from: "ayushrai7533@gmail.com",
-        to: "ayushrai7533@gmail.com",
-        subject: "New Recipient Details",
-        text: `Sender: ${formData.sender}\nRecipient: ${formData.recipient}`
-    };
-
     try {
+        const formData = JSON.parse(event.body);
+
+        let transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: "ayushrai7533@gmail.com",
+                pass: "byiz iotv jjjr vzzy"
+            }
+        });
+
+        let mailOptions = {
+            from: "ayushrai7533@gmail.com",
+            to: "ayushrai7533@gmail.com",
+            subject: "New Recipient Details",
+            text: `Sender: ${formData.sender}\nRecipient: ${formData.recipient}\nCustom Message: ${formData.message || "No custom message provided."}`
+        };
+
         await transporter.sendMail(mailOptions);
         return { statusCode: 200, body: JSON.stringify({ message: "Email Sent" }) };
     } catch (error) {
