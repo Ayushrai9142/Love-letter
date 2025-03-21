b3cf971660ef9bf3e9d861c88a1f54055c21fdd9
Ayushrai9142 <ayushrai7533@gmail.com>
2025-03-22 01:10:53 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: window.VITE_FIREBASE_API_KEY,
+  authDomain: window.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: window.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: window.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: window.VITE_FIREBASE_MESSAGING_ID,
+  appId: window.VITE_FIREBASE_APP_ID
 };
 
 export default firebaseConfig;
