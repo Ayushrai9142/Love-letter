3487d36e79f4956a76beb8661a4736ad6b0f48d2
Ayushrai9142 <ayushrai7533@gmail.com>
2025-03-22 01:50:51 +0530

    Update firebaseconfig.js

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,10 +1,10 @@
-const firebaseConfig = {
-  apiKey: window.VITE_FIREBASE_API_KEY,
-  authDomain: window.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: window.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: window.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: window.VITE_FIREBASE_MESSAGING_ID,
-  appId: window.VITE_FIREBASE_APP_ID
-};
+async function getFirebaseConfig() {
+    const response = await fetch("/.netlify/functions/firebaseConfig");
+    const config = await response.json();
+    return config;
+}
 
-export default firebaseConfig;
+getFirebaseConfig().then((firebaseConfig) => {
+    import { initializeApp } from "firebase/app";
+    const app = initializeApp(firebaseConfig);
+});
