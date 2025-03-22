36a52d1fad8e3f6374f4f9d9c22afc249c8eb43b
Ayushrai9142 <ayushrai7533@gmail.com>
2025-03-22 22:47:21 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,5 +1,6 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { initializeApp } from "firebase/app";
+// Firebase SDK ko properly import karna
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 // ✅ Firebase Config Fetch Function
 async function getFirebaseConfig() {
